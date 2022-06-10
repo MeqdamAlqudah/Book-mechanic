b60b7b18cda73a072bc268c193a3be7a6fc67e1a
Yerimah <danielyerimah@yahoo.com>
2022-06-10 14:24:44 +0100

    Create migration files

---
+++ db/migrate/20220610130624_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.integer :phone
+      t.string :profession
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
