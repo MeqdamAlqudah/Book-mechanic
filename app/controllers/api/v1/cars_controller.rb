5655844c1ae3414c7a7c9e4d1b013f4088f1cb12
Yerimah <danielyerimah@yahoo.com>
2022-06-13 13:49:12 +0100

    Create car controller

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,9 +1,19 @@
 class Api::V1::CarsController < ApplicationController
-  def index; end
+  def index
+    cars = Car.all
+  end
 
-  def create; end
+  def create
+    car = Car.new(car_params)
+  end
 
   def update; end
 
   def delete; end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :registration, :user_id)
+  end
 end
