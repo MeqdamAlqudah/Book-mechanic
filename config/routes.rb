d665f68af673eab93e32e131d8967b45ded22c2c
Mphatso Lichapa <lichapau@gmail.com>
2022-06-13 13:02:26 +0200

    updated config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-  # devise_for :users,
-  #   controllers: {
-  #     sessions: 'users/sessions',
-  #     registrations: 'users/registrations'
-  # }
-
   namespace :api do
-    namespace :v1 do
-    
-      resources :users do
-        resources :cars
-        resources :appointment
-      end
+    namespace :v1 do    
+      resources :users
+      resources :cars      
+      resources :appointment
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
