a27cc5b4752a7159e6283a9146827d7e649a3c53
unknown <memuq56@gmail.com>
2022-06-13 13:28:43 -0700

    Add cars and appointment controller functionality

--- app/controllers/api/v1/appointment_controller.rb
+++ app/controllers/api/v1/appointment_controller.rb
@@ -1,9 +1,35 @@
 class Api::V1::AppointmentController < ApplicationController
-  def index; end
+  def index
+    appointments = Appointment.all
+  end
 
-  def create; end
+  def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save 
+      render json: appointments
+    else
+      render json: appointments.error
+    end
+   end
 
-  def update; end
+  def update
+    appointment = Appointment.find(params[:id])
+    if car.update(appointment_params)
+      render json: appointments
+    else  
+      render json: appointment_params.error
+    end
+   end
 
-  def delete; end
+   def destroy
+    appointment = Appointment.find(params[:id])
+    appointment.destroy
+    head :no_content, status: :ok
+   end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit( :description,:date)
+  end
 end
--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,15 +1,36 @@
 class Api::V1::CarsController < ApplicationController
   def index
     cars = Car.all
+    render json: cars
   end
 
+  def show 
+    render json: cars
+  end
+  
   def create
     car = Car.new(car_params)
+    if car.save 
+      render json: cars
+    else
+      render json: cars.error
+    end
   end
 
-  def update; end
+  def update
+    car = Car.find(params[:id])
+    if car.update(car_params)
+      render json: cars
+    else  
+      render json: cars.error
+    end
+   end
 
-  def delete; end
+  def destroy
+    car = Car.find(params[:id])
+    car.destroy
+    head :no_content, status: :ok
+   end
 
   private
 
