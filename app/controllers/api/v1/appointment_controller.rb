7c6c4b4119fc0e343e024bb57e1682cb41c7eb83
Yerimah <danielyerimah@yahoo.com>
2022-06-13 17:40:08 +0100

    install Rswag

--- app/controllers/api/v1/appointment_controller.rb
+++ app/controllers/api/v1/appointment_controller.rb
@@ -5,31 +5,31 @@ class Api::V1::AppointmentController < ApplicationController
 
   def create
     appointment = Appointment.new(appointment_params)
-    if appointment.save 
+    if appointment.save
       render json: appointments
     else
       render json: appointments.error
     end
-   end
+  end
 
   def update
     appointment = Appointment.find(params[:id])
     if car.update(appointment_params)
       render json: appointments
-    else  
+    else
       render json: appointment_params.error
     end
-   end
+  end
 
-   def destroy
+  def destroy
     appointment = Appointment.find(params[:id])
     appointment.destroy
     head :no_content, status: :ok
-   end
+  end
 
   private
 
   def appointment_params
-    params.require(:appointment).permit( :description,:date)
+    params.require(:appointment).permit(:description, :date)
   end
 end
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-# Be sure to restart your server when you modify this file.
-
-# Configure parameters to be filtered from the log file. Use this to limit dissemination of
-# sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
-# notations and behaviors.
-Rails.application.config.filter_parameters += %i[
-  passw secret token _key crypt salt certificate otp ssn
-]
-=======
 # Be sure to restart your server when you modify this file.
 
 # Configure parameters to be filtered from the log file. Use this to limit dissemination of
@@ -16,4 +6,3 @@ Rails.application.config.filter_parameters += %i[
 Rails.application.config.filter_parameters += [
   :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
 ]
->>>>>>> 996569744ec7d3dea935e4e116089d0a57b548f5
