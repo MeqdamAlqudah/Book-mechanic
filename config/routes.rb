cda5310345f0f2dcb6f611377c1f2fbbaf0f8f14
Yerimah <danielyerimah@yahoo.com>
2022-06-20 00:23:34 +0100

    install devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
 
   namespace :api do
     namespace :v1 do
